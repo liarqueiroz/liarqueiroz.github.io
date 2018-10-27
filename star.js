01efb3ca2c7cf00b0050806880512da770204c83
Ana Livia Queiroz <liarqueiroz@gmail.com>
2018-10-27 14:54:28 -0300

    updates lines to polyline and reorder to make it easier to understand.

--- star.js
+++ star.js
@@ -27,14 +27,6 @@ function CalculateStarPoints(centerX, centerY, arms, outerRadius, innerRadius)
    return results;
 }
 
-function createLine(lineElement, point1, point2) {
-    lineElement.setAttribute('x1', point1.x);
-    lineElement.setAttribute('y1', point1.y);
-
-    lineElement.setAttribute('x2', point2.x);
-    lineElement.setAttribute('y2', point2.y);
-}
-
 document.addEventListener("DOMContentLoaded", function(event) {
 
     var svgEl = document.getElementsByTagName('svg')[0],
@@ -42,15 +34,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
         outerRadius = 50,
         innerRadius = 25,
         centers = [
-            {x: -150, y: 940},
+            {x: 640, y: 470},
+            {x: 780, y: 640},
+            {x: 800, y: 900},
             {x: 115, y: 840},
+            {x: -170, y: 870},
             {x: 75, y: 540},
-            {x: 1100, y: 750},
-            {x: 1000, y: 590},
-            {x: 790, y: 470},
-            {x: 1090, y: 200},
-            {x: 930, y: 70},
-            {x: 740, y: 300}
+            {x: 670, y: 300},
+            {x: 900, y: 180},
+            {x: 970, y: 290}
         ];
 
     centers.forEach(function(center, index) {
@@ -61,13 +53,17 @@ document.addEventListener("DOMContentLoaded", function(event) {
         star.setAttribute('points', CalculateStarPoints(center.x, center.y, arms, outerRadius, innerRadius));
     });
 
-    createLine(document.getElementById('line1'), centers[0], centers[1]);
-    createLine(document.getElementById('line2'), centers[0], centers[2]);
-    createLine(document.getElementById('line3'), centers[1], centers[3]);
-    createLine(document.getElementById('line4'), centers[3], centers[4]);
-    createLine(document.getElementById('line5'), centers[4], centers[5]);
-    createLine(document.getElementById('line6'), centers[5], centers[8]);
-    createLine(document.getElementById('line7'), centers[2], centers[5]);
-    createLine(document.getElementById('line8'), centers[8], centers[7]);
-    createLine(document.getElementById('line9'), centers[7], centers[6]);
+    var path = document.getElementById('path'),
+        points = centers[0].x + ',' + centers[0].y + ' ' +
+                 centers[1].x + ',' + centers[1].y + ' ' +
+                 centers[2].x + ',' + centers[2].y + ' ' +
+                 centers[3].x + ',' + centers[3].y + ' ' +
+                 centers[4].x + ',' + centers[4].y + ' ' +
+                 centers[5].x + ',' + centers[5].y + ' ' +
+                 centers[0].x + ',' + centers[0].y + ' ' +
+                 centers[6].x + ',' + centers[6].y + ' ' +
+                 centers[7].x + ',' + centers[7].y + ' ' +
+                 centers[8].x + ',' + centers[8].y;
+
+    path.setAttribute('points', points);
 });
