60fdcf61dab0de899b33028b5445065fc4d398e2
Ana Livia Queiroz <liarqueiroz@hotmail.com>
2018-10-29 20:34:10 -0300

    adds leo constellation to constellations html

--- constellations.js
+++ constellations.js
@@ -25,15 +25,31 @@ document.addEventListener("DOMContentLoaded", function(event) {
     }
 
     var leoMinorConstellation = {
-        name: 'leo-minor',
-        starCenters: [
-            {x: 5, y: 1070},
-            {x: 100, y: 1270},
-            {x: 200, y: 1470},
-            {x: 90, y: 1370}
-        ],
-        path: [0, 1, 2, 3, 1]
-    }
+            name: 'leo-minor',
+            starCenters: [
+                {x: 5, y: 1000},
+                {x: 150, y: 1270},
+                {x: 250, y: 1470},
+                {x: 140, y: 1370}
+            ],
+            path: [0, 1, 2, 3, 1]
+        },
+        leoConstellation = {
+            name: 'leo',
+            starCenters: [
+                {x: 630, y: 860},
+                {x: 550, y: 1000},
+                {x: 750, y: 1200},
+                {x: 930, y: 1150},
+                {x: 1000, y: 1000},
+                {x: 1170, y: 890},
+                {x: 1260, y: 1540},
+                {x: 1370, y: 1740},
+                {x: 1060, y: 1600}
+            ],
+            path: [0, 1, 2, 3, 4, 5, 6, 7, 8, 3]
+        };
 
     createConstellation(leoMinorConstellation);
+    createConstellation(leoConstellation);
 });
